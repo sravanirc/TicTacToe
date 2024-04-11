1947cdd72ea5d4db47e9898f67fb654248db1d71
sravanirc <sravani.rc@gmail.com>
2024-04-11 21:26:51 +0100

    first commit for TicTacToe game

---
+++ tictactoe.js
@@ -0,0 +1,72 @@
+let cells = document.querySelectorAll(".cell");
+let message = document.getElementById("message");
+message.textContent="Let's Begin!";
+
+let currentPlayer = "X";
+let winner = null;
+let moves = 0;
+
+function handleClick(event){
+    let cell = event.target;
+    
+    if(cell.textContent==="" && !winner){
+        cell.textContent = currentPlayer;
+        moves++;
+    }
+    
+    if(checkWin()){
+        winner = currentPlayer;
+    }else if(moves>=9){
+        winner = "Draw";
+    }else {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
+
+  if(winner){
+    displayWinner();
+
+  }
+}
+
+function checkWin(){
+    let winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
+    
+    for(let pattern of winPatterns){
+         let [a,b,c] = pattern;
+
+        if(cells[a].textContent != "" && 
+           cells[a].textContent==cells[b].textContent &&
+           cells[b].textContent==cells[c].textContent){
+            console.log(cells[a].textContent);
+            return true;
+        }
+    }
+}
+
+function displayWinner(){
+    if(winner==="Draw"){
+        message.textContent =  "It's a Draw!";
+    }
+    else {
+        message.textContent = `Player ${winner} wins!`;
+    }
+}
+
+function reset(){
+    cells.forEach((cell)=>(cell.textContent=""));
+    winner=null;
+    moves=0;
+    message.textContent="Let's Go Again!";
+}
+
+cells.forEach((cell)=> cell.addEventListener("click",handleClick));
+document.getElementById("reset").addEventListener("click",reset);
